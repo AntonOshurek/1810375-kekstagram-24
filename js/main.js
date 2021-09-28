e64c2e5182957bc4af1e4ccd322fc4b9dcab7f0d
AntonOshurek <oshurekweb@gmail.com>
2021-09-28 18:32:37 +0200

    adding string length check function

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const maxStringValue = 140;
+const testString = 'test string for my code';
+
+function testingString(max, string) {
+  return string.length <= max;
+}
+testingString(maxStringValue, testString);
+
+
