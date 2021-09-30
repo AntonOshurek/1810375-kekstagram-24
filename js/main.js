48de8cdd3502ac28f111530fd86b1b4d1e5670ef
AntonOshurek <oshurekweb@gmail.com>
2021-09-30 21:43:13 +0200

    correction of names of functions and constants

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-const maxStringValue = 140;
-const testString = 'test string for my code';
+const MAXSTRINGVALUE = 140;
+const TESTSTRING = 'test string for my code';
 
-function testingString(max, string) {
+function testString(max, string) {
   return string.length <= max;
 }
-testingString(maxStringValue, testString);
+testString(MAXSTRINGVALUE, TESTSTRING);
 
-function randomint(randomMinValue, randomMaxValue) {
+function getRandomInt(randomMinValue, randomMaxValue) {
   randomMinValue >= randomMaxValue ? randomMinValue = randomMaxValue - 1 : false;
 
   if(((Math.sign(randomMinValue) !== -1) && (Math.sign(randomMinValue) !== -1))) {
     return Math.floor(Math.random() * (randomMaxValue - randomMinValue + 1)) + randomMinValue;
   }
 }
-randomint(40, 40);
+console.log(getRandomInt(-50, 40));
