d095097fb02aac2d6b4161859df7235d02d3ac19
AntonOshurek <oshurekweb@gmail.com>
2021-10-01 17:15:47 +0200

    correctinggetRandom function

--- js/main.js
+++ js/main.js
@@ -5,15 +5,10 @@ const testString = (max, string) => string.length <= max;
 testString(MAX_STRING_VALUE, TEST_STRING_LENGTH);
 
 const getRandomInt = (minValue, maxValue) => {
-  if (minValue >= maxValue) {
-    return new Error('minValue of getRandomInt function') ;
+  if((Math.sign(minValue) === -1) || (Math.sign(maxValue) === -1) || (isNaN(minValue)) || (isNaN(maxValue)) || minValue >= maxValue) {
+    throw new Error('minValue or maxValue they have the wrong value');
   }
-
-  if((Math.sign(minValue) === -1) || (Math.sign(maxValue) === -1) || (isNaN(minValue)) || (isNaN(maxValue)) || (typeof(minValue) === 'string') || (typeof(maxValue) === 'string')) {
-    return new Error('minValue or maxValue they have the wrong value') ;
-  } else {
-    return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
-  }
+  return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
 };
 
-console.log(getRandomInt(40, 41));
+getRandomInt(50, 45);
