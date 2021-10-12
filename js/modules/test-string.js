2a2774c4a1b1052a0a9df277f68ffac3d85cfdcf
AntonOshurek <oshurekweb@gmail.com>
2021-10-12 21:09:13 +0200

    removing unused variables and calling a function

--- js/modules/test-string.js
+++ js/modules/test-string.js
@@ -1,6 +1,1 @@
-const MAX_STRING_VALUE = 140;
-const TEST_STRING = 'test string for my code';
-
 export const testStringLength = (max, string) => string.length <= max;
-
-testStringLength(MAX_STRING_VALUE, TEST_STRING);
