28feee6cdf714e32e6bcfe018c07a848420a7cea
AntonOshurek <oshurekweb@gmail.com>
2021-10-12 15:24:46 +0200

    correcting test-tring function export

--- js/modules/test-string.js
+++ js/modules/test-string.js
@@ -1,8 +1,6 @@
-export default function testString() {
-  const MAX_STRING_VALUE = 140;
-  const TEST_STRING = 'test string for my code';
+const MAX_STRING_VALUE = 140;
+const TEST_STRING = 'test string for my code';
 
-  const testStringLength = (max, string) => string.length <= max;
+export const testStringLength = (max, string) => string.length <= max;
 
-  testStringLength(MAX_STRING_VALUE, TEST_STRING);
-}
+testStringLength(MAX_STRING_VALUE, TEST_STRING);
