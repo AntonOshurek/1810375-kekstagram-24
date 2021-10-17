12dc32385798f2582104a73276f7cbef4edd1c45
AntonOshurek <oshurekweb@gmail.com>
2021-10-17 11:19:56 +0200

    show all posts in page

--- js/modules/show-pictures.js
+++ js/modules/show-pictures.js
@@ -1,20 +1,21 @@
+import getPosts from './get-posts.js';
+
 export default function showPictures() {
   const PICTURE_BLOCK = document.querySelector('.pictures');
   const PICTURE_TEMPLATE = document.querySelector('#picture');
-
+  const Posts = getPosts();
 
   const FRAGMENT = new DocumentFragment();
 
-  for(let i = 0; i < 5; i++) {
+  for(let i = 0; i < Posts.length; i++) {
     const TEMPLATE_ITEM = PICTURE_TEMPLATE.content.cloneNode(true);
 
-    TEMPLATE_ITEM.querySelector('.picture__img').src = `${i}-ind`;
-    TEMPLATE_ITEM.querySelector('.picture__comments').innerText = 'sdfsdf';
-    TEMPLATE_ITEM.querySelector('.picture__likes').innerText = 'hujsdfsdfsdf';
+    TEMPLATE_ITEM.querySelector('.picture__img').src = `${Posts[i].url}`;
+    TEMPLATE_ITEM.querySelector('.picture__comments').innerText = `${Posts[i].comments.length}`;
+    TEMPLATE_ITEM.querySelector('.picture__likes').innerText = `${Posts[i].likes}`;
 
     FRAGMENT.append(TEMPLATE_ITEM);
   }
 
-  console.log(FRAGMENT);
-
+  PICTURE_BLOCK.append(FRAGMENT);
 }
