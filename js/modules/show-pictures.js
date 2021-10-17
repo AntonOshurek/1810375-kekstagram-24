c523b5b1b6d6b5178e783948ab34e694fba19d98
AntonOshurek <oshurekweb@gmail.com>
2021-10-17 11:28:44 +0200

    code verification by criteria

--- js/modules/show-pictures.js
+++ js/modules/show-pictures.js
@@ -1,18 +1,18 @@
 import getPosts from './get-posts.js';
 
-export default function showPictures() {
-  const PICTURE_BLOCK = document.querySelector('.pictures');
-  const PICTURE_TEMPLATE = document.querySelector('#picture');
-  const posts = getPosts();
+const PICTURE_BLOCK = document.querySelector('.pictures');
+const PICTURE_TEMPLATE = document.querySelector('#picture');
+const posts = getPosts();
+const FRAGMENT = new DocumentFragment();
 
-  const FRAGMENT = new DocumentFragment();
+export default function showPictures() {
 
   for(let i = 0; i < posts.length; i++) {
     const TEMPLATE_ITEM = PICTURE_TEMPLATE.content.cloneNode(true);
 
     TEMPLATE_ITEM.querySelector('.picture__img').src = `${posts[i].url}`;
-    TEMPLATE_ITEM.querySelector('.picture__comments').innerText = `${posts[i].comments.length}`;
-    TEMPLATE_ITEM.querySelector('.picture__likes').innerText = `${posts[i].likes}`;
+    TEMPLATE_ITEM.querySelector('.picture__comments').textContent = `${posts[i].comments.length}`;
+    TEMPLATE_ITEM.querySelector('.picture__likes').textContent = `${posts[i].likes}`;
 
     FRAGMENT.append(TEMPLATE_ITEM);
   }
