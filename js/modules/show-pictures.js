bcb4ea2a5f9cc8e5f0f188de160c984a66987963
AntonOshurek <oshurekweb@gmail.com>
2021-10-17 11:03:28 +0200

    adding basic logick

--- js/modules/show-pictures.js
+++ js/modules/show-pictures.js
@@ -1,3 +1,20 @@
 export default function showPictures() {
-  
+  const PICTURE_BLOCK = document.querySelector('.pictures');
+  const PICTURE_TEMPLATE = document.querySelector('#picture');
+
+
+  const FRAGMENT = new DocumentFragment();
+
+  for(let i = 0; i < 5; i++) {
+    const TEMPLATE_ITEM = PICTURE_TEMPLATE.content.cloneNode(true);
+
+    TEMPLATE_ITEM.querySelector('.picture__img').src = `${i}-ind`;
+    TEMPLATE_ITEM.querySelector('.picture__comments').innerText = 'sdfsdf';
+    TEMPLATE_ITEM.querySelector('.picture__likes').innerText = 'hujsdfsdfsdf';
+
+    FRAGMENT.append(TEMPLATE_ITEM);
+  }
+
+  console.log(FRAGMENT);
+
 }
