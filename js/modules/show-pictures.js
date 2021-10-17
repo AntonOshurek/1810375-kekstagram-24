3e771a1145438e7dbe0f73e59589f4a549ba7ab4
AntonOshurek <oshurekweb@gmail.com>
2021-10-17 11:21:54 +0200

    text correcting

--- js/modules/show-pictures.js
+++ js/modules/show-pictures.js
@@ -3,16 +3,16 @@ import getPosts from './get-posts.js';
 export default function showPictures() {
   const PICTURE_BLOCK = document.querySelector('.pictures');
   const PICTURE_TEMPLATE = document.querySelector('#picture');
-  const Posts = getPosts();
+  const posts = getPosts();
 
   const FRAGMENT = new DocumentFragment();
 
-  for(let i = 0; i < Posts.length; i++) {
+  for(let i = 0; i < posts.length; i++) {
     const TEMPLATE_ITEM = PICTURE_TEMPLATE.content.cloneNode(true);
 
-    TEMPLATE_ITEM.querySelector('.picture__img').src = `${Posts[i].url}`;
-    TEMPLATE_ITEM.querySelector('.picture__comments').innerText = `${Posts[i].comments.length}`;
-    TEMPLATE_ITEM.querySelector('.picture__likes').innerText = `${Posts[i].likes}`;
+    TEMPLATE_ITEM.querySelector('.picture__img').src = `${posts[i].url}`;
+    TEMPLATE_ITEM.querySelector('.picture__comments').innerText = `${posts[i].comments.length}`;
+    TEMPLATE_ITEM.querySelector('.picture__likes').innerText = `${posts[i].likes}`;
 
     FRAGMENT.append(TEMPLATE_ITEM);
   }
