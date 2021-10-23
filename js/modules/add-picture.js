2435faa5e7d343fe6db0f1a604a24d4e89830d30
AntonOshurek <oshurekweb@gmail.com>
2021-10-23 17:18:44 +0200

    adding check validity functions

--- js/modules/add-picture.js
+++ js/modules/add-picture.js
@@ -1,8 +1,34 @@
+import {testStringLength} from './test-string.js';
+
 export default function addPicture() {
   const body = document.querySelector('body');
   const uploadFile = document.querySelector('#upload-file');
   const imageUpload = document.querySelector('.img-upload__overlay');
   const imageUploadCancel = document.querySelector('.img-upload__cancel');
+  const textHashtags = document.querySelector('.text__hashtags');
+  const textDescription = document.querySelector('.text__description');
+  const uploadSubmit = document.querySelector('.img-upload__submit');
+
+  const checkCommentValidity = () => {
+    const MAX_COMMENT_LENGTH = 140;
+    if (!testStringLength(MAX_COMMENT_LENGTH, textDescription.value)) {
+      textDescription.setCustomValidity('до 140 символов');
+    } else {
+      textDescription.setCustomValidity('');
+    }
+    textDescription.reportValidity();
+  };
+
+  const checkHashValidity = () => {
+    const reg = /(#[a-z0-9][a-z0-9\-_]*)/ig;
+
+    if (!reg.test(textHashtags.value)) {
+      textHashtags.setCustomValidity('некорректный тег');
+    } else {
+      textHashtags.setCustomValidity('');
+    }
+    textHashtags.reportValidity();
+  };
 
   const onUploadEscKeydown = (evt) => {
     if (evt.key === 'Escape') {
@@ -21,6 +47,9 @@ export default function addPicture() {
 
     document.addEventListener('keydown', onUploadEscKeydown);
     imageUploadCancel.addEventListener('click', onCancelClick);
+
+    textDescription.addEventListener('input', checkCommentValidity);
+    textHashtags.addEventListener('input', checkHashValidity);
   }
 
   function uploadModalClose () {
@@ -29,9 +58,14 @@ export default function addPicture() {
 
     document.removeEventListener('keydown', onCancelClick);
     imageUploadCancel.removeEventListener('click', onCancelClick);
+
+    textDescription.removeEventListener('input', checkCommentValidity);
+    textHashtags.removeEventListener('input', checkHashValidity);
+    uploadFile.value = '';
   }
 
   uploadFile.addEventListener('change', () => {
     uploadModalOpen();
   });
+
 }
