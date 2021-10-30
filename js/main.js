7faf5eda373f832158fdd8f8f09ae11cb2223e82
AntonOshurek <oshurekweb@gmail.com>
2021-10-30 11:20:53 +0200

    adding scale img function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import showPictures from './modules/show-pictures.js';
 import addPicture from './modules/add-picture.js';
+import photoEditing from './modules/photo-editing.js';
 
 showPictures();
 addPicture();
+photoEditing();
